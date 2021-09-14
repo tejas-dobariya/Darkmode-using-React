601e937ad8a11e7abdeb23631790e5c971a21a19
tejas-dobariya <a9510262243@gmail.com>
2021-09-14 10:47:03 +0530

    Files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
+import About from "./components/About";
+
+
+function App() {
+  return (
+    <>
+    <Navbar title="Textutils" aboutTextutils="About Textutils"/>
+    <div className="container">
+    {/* <TextForm heading="Enter your text here to be Analyzed"/> */}
+   <About/> 
+    </div>
+    </>
+  );
+}
+
+export default App;
+
